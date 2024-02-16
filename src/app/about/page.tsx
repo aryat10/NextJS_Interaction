b57a2edad2466d9fcf6fc00de1f434fde516fca8
Aryat Srivastava <107941072+aryat10@users.noreply.github.com>
2024-02-16 23:05:48 +0530

    Add files via upload

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About()
+{
+    return (
+        <div>
+            <h1>About page rendering successfully ! </h1>
+            <img src="https://live.staticflickr.com/834/42533298285_c85af9be26_c.jpg" alt="" />
+        </div>
+    )
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile()
+{
+    return (
+        <div>
+            <h1> page is rendering successfully 💕</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXVEGRNENy_ngfCm9At7Xr87aE4h6euCEP7Q&usqp=CAU" alt="photo"/>
+        </div>
+    )
+}
+
